2330c9486c39bb0367594cb7970efda4f1a507b3
Ivar Abrahamsen <ivar@flurdy.com>
2016-08-07 01:58:56 +0100

    Primitives

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := """sander"""
+organization  := "com.flurdy"
+
+scalaVersion := "2.11.6"
+
+version := "0.1.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
---
+++ src/main/scala/primitives.scala
@@ -0,0 +1,20 @@
+package com.flurdy.sander
+
+import scala.concurrent.Future
+
+package object primitives {
+
+   implicit class OptionAsFuture[T](value: Option[T]) {
+      def future: Future[T] = value.fold[Future[T]]
+         { Future.failed(new NoSuchElementException) }
+         { o => Future.successful(o) }
+   }
+
+   implicit class BooleanFold(boolean: Boolean) {
+      def fold[B](l: B)(r: B => B): B = if(boolean) r(l) else l
+   }
+
+   implicit class BooleanSome(boolean: Boolean) {
+      def some: Option[Boolean] = if(boolean) Some(true) else None
+   }
+}
